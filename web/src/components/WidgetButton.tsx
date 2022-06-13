bac680e0d9e9b5a7f5a1da0e6e780af989d72d75
Athirson Silva <athirson07@gmail.com>
2022-06-13 15:28:34 -0300

    Correções das correções

--- web/src/components/WidgetButton.tsx
+++ web/src/components/WidgetButton.tsx
@@ -1,18 +1,18 @@
 import { ChatTeardropDots } from 'phosphor-react';
 import { Popover } from '@headlessui/react';
-import { WidgetForm } from './WidgetForm';
+import { WidgetForm } from './WidgetForm/Index';
 
 //React observa os estados, e quando o estados mudarem o React renderiza o componante novamente
 
-export function WidgetButton () {
-        /*Nome da variável do estado, função que altera o estado
-    const [isWidgetOpen, setIsWidgetOpen] = useState(false)
+export function WidgetButton() {
+    /*Nome da variável do estado, função que altera o estado
+const [isWidgetOpen, setIsWidgetOpen] = useState(false)
 
-    function toggleWidgetVisibility() {
-        setIsWidgetOpen(!isWidgetOpen);
-    }
-    Mas usaremos a biblioteca headlessui para cuidar da acessibilidade e dessa função 
-    */
+function toggleWidgetVisibility() {
+    setIsWidgetOpen(!isWidgetOpen);
+}
+Mas usaremos a biblioteca headlessui para cuidar da acessibilidade e dessa função 
+*/
 
     return (
         <Popover className='absolute bottom-4 right-4 md:bottom-8 md:right-8 flex flex-col items-end'>
@@ -21,8 +21,8 @@ export function WidgetButton () {
             </Popover.Panel>
 
             <Popover.Button className='bg-brand-500 text-white rounded-full px-3 h-12 flex items-center group'>
-                <ChatTeardropDots className='w-6 h-6 '/>
- 
+                <ChatTeardropDots className='w-6 h-6 ' />
+
                 <span className='max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear'>
                     <span className='pl-2'></span>
                     Feedback
--- web/src/components/WidgetForm/Index.tsx
+++ web/src/components/WidgetForm/Index.tsx
@@ -1,4 +1,4 @@
-import { CloseButton} from '../CloseButton'
+import { CloseButton } from '../CloseButton'
 
 import bugImageUrl from "../../assets/Bug.svg"
 import ideaImageUrl from '../../assets/Idea.svg'
@@ -33,7 +33,7 @@ export const feedbackTypes = {
 
 }
 //type of seleciona o tipo de cada elemento dos tipos, e o keyof a key destes => "BUG" | "IDEA" | "OTHER"
-export type FeedbackType = keyof typeof feedbackTypes; 
+export type FeedbackType = keyof typeof feedbackTypes;
 
 //{ Object.entries(feedbackTypes) } => [ ['BUG', {conteúdo do objeto}], [...], [...]]
 export function WidgetForm() {
@@ -48,20 +48,19 @@ export function WidgetForm() {
     return (
         //calc(100vw-2rem calculo para definir o tamanho do form, 100 viewport width - 2rem, md:w-auto tamanhos de tela médios+
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-            { feedbackSent ?
-            <FeedbackSucessStep onFeedbackRestartRequested={restartFeedbackHandle} /> :
-            <>
-                {!feedbackType ? (
-                    <FeedbackTypeStep setFeedbackTypeHandler = {setFeedbackType}  />
-                ) : (
-                    <FeedbackContentStep 
-                    feedbackType={feedbackType}
-                    onFeedbackRestart={restartFeedbackHandle}
-                    onFeedbackSent={() => setFeedbackSent(true)} />
-                ) }
-            </>
-             }
-            
+            {feedbackSent ?
+                <FeedbackSucessStep onFeedbackRestartRequested={restartFeedbackHandle} /> :
+                <>
+                    {!feedbackType ? (
+                        <FeedbackTypeStep setFeedbackTypeHandler={setFeedbackType} />
+                    ) : (
+                        <FeedbackContentStep
+                            feedbackType={feedbackType}
+                            onFeedbackRestart={restartFeedbackHandle}
+                            onFeedbackSent={() => setFeedbackSent(true)} />
+                    )}
+                </>
+            }
 
             <footer className="text-xs text-neutral-400">
                 <span>Feito com ♥ pela <a className="underline underline-offset-2" target="_blank" href="https://www.rocketseat.com.br">Rocketseat</a> e <a className="underline underline-offset-2" target="_blank" href="https://www.linkedin.com/in/athirson/" >Eu</a></span>
