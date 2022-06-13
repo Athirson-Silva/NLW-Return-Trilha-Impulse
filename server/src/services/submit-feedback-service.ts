cca67d8a9427e2ef97734e2b3648b2ec590ac598
Athirson Silva <athirson07@gmail.com>
2022-06-13 16:20:38 -0300

    Correção submit feedback

--- server/src/services/submit-feedback-service.ts
+++ server/src/services/submit-feedback-service.ts
@@ -49,7 +49,7 @@ export class SubmitFeedbackService {
                 `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
                 `<p>Tipo de feedback: ${type}</p`,
                 `<p>Comentário: ${comment}</p`,
-                screenshot ? `<img src="${screenshot}` : null,
+                screenshot ? `<img src="${screenshot}">` : null,
                 `</div>`
             ].join('\n')
         })
